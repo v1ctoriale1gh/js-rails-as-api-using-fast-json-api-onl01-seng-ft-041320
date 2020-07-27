912a3e822f3a24cb3f7baea46327e825d75560e2
github username <github email address>
2020-07-27 14:12:14 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,7 @@
 class SightingsController < ApplicationController
   def show
     sighting = Sighting.find(params[:id])
+    
     render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
   end
 end
